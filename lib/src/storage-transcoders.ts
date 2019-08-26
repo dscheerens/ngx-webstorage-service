9ac3e0c5d532b7a5d3abb0d67a9a78d15808e698
Dirk Luijk <mail@dirkluijk.nl>
2019-08-26 17:38:49 +0200

    feat(transcoders): add transcoders for dates

--- lib/src/storage-transcoders.ts
+++ lib/src/storage-transcoders.ts
@@ -59,6 +59,32 @@ export class NumberStorageTranscoder implements StorageTranscoder<number> {
     }
 }
 
+/** Transcoder that encodes/decodes `Date` values to ISO strings. */
+export class DateIsoStorageTranscoder implements StorageTranscoder<Date> {
+    public encode(value: Date): string {
+        return value.toISOString();
+    }
+
+    public decode(value: string): Date | undefined {
+        const timestamp = Date.parse(value);
+
+        return isNaN(timestamp) ? undefined : new Date(timestamp);
+    }
+}
+
+/** Transcoder that encodes/decodes `Date` values to epoch timestamps. */
+export class DateEpochStorageTranscoder implements StorageTranscoder<Date> {
+    public encode(value: Date): string {
+        return value.valueOf().toString();
+    }
+
+    public decode(value: string): Date | undefined {
+        const timestamp = parseInt(value, 10);
+
+        return isNaN(timestamp) ? undefined : new Date(timestamp);
+    }
+}
+
 /** A set of storage transcoders. */
 export const StorageTranscoders = {
     /** Transcoder that encodes values as JSON strings. */
@@ -71,5 +97,11 @@ export const StorageTranscoders = {
     BOOLEAN: new BooleanStorageTranscoder() as StorageTranscoder<boolean>,
 
     /** Transcoder that encodes/decodes `number` values. */
-    NUMBER: new NumberStorageTranscoder() as StorageTranscoder<number>
+    NUMBER: new NumberStorageTranscoder() as StorageTranscoder<number>,
+
+    /** Transcoder that encodes/decodes `Date` values into ISO strings. */
+    DATE_ISO_STRING: new DateIsoStorageTranscoder() as StorageTranscoder<Date>,
+
+    /** Transcoder that encodes/decodes `Date` values into epoch timestamps. */
+    DATE_EPOCH_TIME: new DateEpochStorageTranscoder() as StorageTranscoder<Date>
 };
